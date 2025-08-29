6afaa5fee6e60f67bcd75c13e0aefbe15bfc0028
jobayer561 <jobayer3085@gmail.com>
2025-08-29 23:34:17 +0600

    button issue solved

--- index/index.js
+++ index/index.js
@@ -9,47 +9,50 @@ getElement("card-box").addEventListener("click", function (e) {
     currHeart++;
     totalHeart.innerText = currHeart;
   }
-  
-  if (e.target.className.includes("copy-btn")) {
-    const copyButton = e.target;
-    const number = copyButton.parentNode.parentNode.children[3].innerText;
+});
+
+const copyButtons = document.querySelectorAll(".copy-btn");
+for (const btn of copyButtons) {
+  btn.addEventListener("click", function () {
+    const number = btn.parentNode.parentNode.children[3].innerText;
     navigator.clipboard.writeText(number);
     const totalCopy = getElement("copy-count");
     let currCopy = Number(totalCopy.innerText);
     currCopy++;
     totalCopy.innerText = currCopy;
     alert(`Number Copied: ${number}`);
-  }
-  if (e.target.className.includes("call-btn")) {
+  });
+}
+const callButtons = document.querySelectorAll(".call-btn");
+for (const callBtn of callButtons) {
+  callBtn.addEventListener("click", function () {
     const totalCoin = getElement("coin");
     let currCoin = Number(totalCoin.innerText);
-    if (currCoin <= 0) {
-      alert("Insufficient coin.Minimum 20 coins required to make a call");
+
+    if (currCoin < 20) {
+      alert("Insufficient coin. Minimum 20 coins required to make a call");
       return;
     }
-    const heartButton = e.target;
-    const title = heartButton.parentNode.parentNode.children[1].innerText;
-    const num = heartButton.parentNode.parentNode.children[3].innerText;
+    const title = callBtn.parentNode.parentNode.children[1].innerText;
+    const num = callBtn.parentNode.parentNode.children[3].innerText;
     currCoin = currCoin - 20;
     totalCoin.innerText = currCoin;
     alert(`📞 Calling ${title} ${num}...`);
-        const showContainer = getElement("show-container");
-
+    const showContainer = getElement("show-container");
     const show = document.createElement("div");
     show.innerHTML = `
-     <div class="bg-[#FAFAFA]  rounded-xl flex justify-between items-center mt-3 p-4">
-                  <div class="">
-                    <h2 class="font-bold">${title}</h2>
-                    <h2 class="">${num}</h2>
-                  </div>
-                  <div class="whitespace-nowrap">${new Date().toLocaleTimeString()}</div>
-            </div>
+      <div class="bg-[#FAFAFA] rounded-xl flex justify-between items-center mt-3 p-4">
+        <div>
+          <h2 class="font-bold">${title}</h2>
+          <h2>${num}</h2>
+        </div>
+        <div class="whitespace-nowrap">${new Date().toLocaleTimeString()}</div>
+      </div>
     `;
-    showContainer.append(show)
-  }
-
+    showContainer.append(show);
+  });
+}
+getElement("button-clear").addEventListener("click", function () {
+  const clearContainer = getElement("show-container");
+  clearContainer.innerHTML = "";
 });
-getElement('button-clear').addEventListener('click',function(){
-    const clearContainer = getElement('show-container')
-    clearContainer.innerHTML = "";
-})

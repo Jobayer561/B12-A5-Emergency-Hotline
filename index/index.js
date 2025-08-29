538d2a0bfa76f885f4077cefe97c0fb265cc3604
jobayer561 <jobayer3085@gmail.com>
2025-08-29 15:04:59 +0600

    call functionality and clear functionality add

--- index/index.js
+++ index/index.js
@@ -19,4 +19,36 @@ getElement("card-box").addEventListener("click", function (e) {
     totalCopy.innerText = currCopy;
     alert(`Number Copied: ${number}`);
   }
+  if (e.target.className.includes("call-btn")) {
+    const totalCoin = getElement("coin");
+    let currCoin = Number(totalCoin.innerText);
+    if (currCoin <= 0) {
+      alert("Insufficient coin.Minimum 20 coins required to make a call");
+      return;
+    }
+    const heartButton = e.target;
+    const title = heartButton.parentNode.parentNode.children[1].innerText;
+    const num = heartButton.parentNode.parentNode.children[3].innerText;
+    currCoin = currCoin - 20;
+    totalCoin.innerText = currCoin;
+    alert(`📞 Calling ${title} ${num}...`);
+        const showContainer = getElement("show-container");
+
+    const show = document.createElement("div");
+    show.innerHTML = `
+     <div class="bg-[#FAFAFA] rounded-xl flex justify-between items-center mt-3 p-3">
+                  <div class="">
+                    <h2 class="font-bold">${title}</h2>
+                    <h2 class="">${num}</h2>
+                  </div>
+                  <div class="whitespace-nowrap">${new Date().toLocaleTimeString()}</div>
+            </div>
+    `;
+    showContainer.append(show)
+  }
+
 });
+getElement('button-clear').addEventListener('click',function(){
+    const clearContainer = getElement('show-container')
+    clearContainer.innerHTML = "";
+})
